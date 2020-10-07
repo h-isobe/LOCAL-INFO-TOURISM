0ee5cdc30714bf796701a7a7bb6794ea6c7be026
磯部浩樹 <h.sep23@gmail.com>
2020-10-07 14:09:07 +0900

    [Add]ゲストログイン

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
