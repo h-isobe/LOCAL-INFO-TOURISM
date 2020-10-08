3f670b8f4cc2e54219b19dc70ec7f045e2bfc4da
磯部浩樹 <h.sep23@gmail.com>
2020-10-08 15:13:26 +0900

    [Add]post_show&edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,26 +6,48 @@ class PostsController < ApplicationController
   end
 
   def create
-
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      redirect_to posts_path(@post)
+      flash[:success] = "投稿しました"
+    else
+      render :new 
+    end
   end
 
   def index
-
+    @posts = Post.all
   end
 
   def show
-
+    @post = Post.find(params[:id])
   end
 
   def edit
-
+    @post = Post.find(params[:id])
   end
 
   def update
-
+    post = Post.find(params[:id])
+    if post.update(post_params)
+      redirect_to post_path(post)
+      flash[:success] = "編集しました"
+    else
+      render :edit
+    end
   end
 
   def destroy
-    
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to posts_path
   end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :prefecture, :body, category_ids: [], post_images_images: [])
+  end
+
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '7637497eb250df0e4c6a2b8eb8f2ead17c3b3503a8dc6b777faa30f93dac62b0d00411dbed7fe10a9d27f406ef04cf6e649ace2eccc24d2d1aa1f689b510d672'
