34622139604bfa82f6ff99a221c57271c875d7cd
磯部浩樹 <h.sep23@gmail.com>
2020-10-10 21:23:49 +0900

    [Add]favorite_ranking

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
+    @all_ranks = Post.find(Favorite.group(:post_id).order('count(post_id) desc').limit(3).pluck(:post_id))
   end
 
   def show
